a6f7edcbf8021e7e45719514624051cb11f5a125
Siddhant Chawla <siddhant.chawla9@gmail.com>
2025-04-04 23:30:44 +0000

    Unique-Ids challenge completed

---
+++ unique-ids/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"os"
+	"strconv"
+
+	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
+)
+
+func main() {
+	n := maelstrom.NewNode()
+
+	i := 0
+	// Register a handler for the "generate" message that responds with an "generate_ok".
+	n.Handle("generate", func(msg maelstrom.Message) error {
+		// Unmarshal the message body as an loosely-typed map.
+		var body map[string]any
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+
+		// Update the message type.
+		body["type"] = "generate_ok"
+		body["id"] = msg.Dest + strconv.Itoa(i)
+
+		i += 1
+
+		// Echo the original message back with the updated message type.
+		return n.Reply(msg, body)
+	})
+
+	// Execute the node's message loop. This will run until STDIN is closed.
+	if err := n.Run(); err != nil {
+		log.Printf("ERROR: %s", err)
+		os.Exit(1)
+	}
+}
